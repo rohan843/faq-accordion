04dd8c6fd5f62ec322535537df3c27c9290ad532
rohan843 <rohan.sharma2934@gmail.com>
2024-03-08 18:22:38 +0530

    fix: re-create para open-close with wrappers and no animation

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ const questions = document.querySelectorAll(".question");
 questions.forEach((element) => {
   element.querySelector(".question-header").addEventListener("click", () => {
     element.classList.toggle("closed");
+    element.classList.toggle("opened");
   });
 });
