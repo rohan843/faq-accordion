656acc2f064f1297c528ca85b64a1f8d2d1f818c
rohan843 <rohan.sharma2934@gmail.com>
2024-03-08 19:46:35 +0530

    chore: auto format code

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const questions = document.querySelectorAll(".question");
 
 questions.forEach((element) => {
-  element.querySelector(".question-link-wrapper").addEventListener("click", (e) => {
-    e.preventDefault();
-    element.classList.toggle("closed");
-    element.classList.toggle("opened");
-    if(element.classList.contains("closed")) {
-      element.classList.remove("opened-anim");
-      element.classList.add("closed-anim");
-    } else {
-      element.classList.remove("closed-anim");
-      element.classList.add("opened-anim");
-    }
-  });
+  element
+    .querySelector(".question-link-wrapper")
+    .addEventListener("click", (e) => {
+      e.preventDefault();
+      element.classList.toggle("closed");
+      element.classList.toggle("opened");
+      if (element.classList.contains("closed")) {
+        element.classList.remove("opened-anim");
+        element.classList.add("closed-anim");
+      } else {
+        element.classList.remove("closed-anim");
+        element.classList.add("opened-anim");
+      }
+    });
 });
